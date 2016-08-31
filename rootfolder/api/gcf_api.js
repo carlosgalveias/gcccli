0cff8e57ba7f52237a07668aa1e6922e58f6bbdb
Carlos Galveias <carlos.galveias@gdev.biz>
2016-08-31 03:09:15 +0100

    added authorization headers

--- rootfolder/api/gcf_api.js
+++ rootfolder/api/gcf_api.js
@@ -24,13 +24,35 @@ models.waterline.initialize(models.config, function(err, models) {
     db.models = models.collections;
     db.connections = models.connections;
 });
+'use strict';
+var models = require('../models');
+var path = require('path');
+
+var db = { models: [], connections: [] };
+
+models.waterline.initialize(models.config, function(err, models) {
+    if (err) {
+        throw err;
+    }
+    db.models = models.collections;
+    db.connections = models.connections;
+});
 
 var api = function(req, res) {
-    res.header('Access-Control-Allow-Origin', '*');
+    // Headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Max-Age', '86400');
+    if (req.method === 'OPTIONS') {
+        res.send();
+    }
+
     var component = req.params['0'].replace(/\/[\s\S]*/, '');
     var id = req.params['0'].match(/\/.*/);
     var method = req.method.toLowerCase();
     var model;
+
 
     // for express compatibility
     if (id) {
@@ -39,19 +61,21 @@ var api = function(req, res) {
     req.params.id = id;
     req.params['0'] = component;
 
+    // First we try to load a custom file, or we use the generic one
     try {
         model = require(path.join(__dirname, '../routers', component + (id ? '-id.js' : '.js')));
     } catch (err) {
         model = require(path.join(__dirname, '../routers', 'generic.js')); // Loads the generic one
     }
     try {
-        model[method]((req, ret) => {
+        model[method](req, ret => {
             res.status(ret.status).send(ret.result);
         });
     } catch (err) {
         console.log(err);
         res.status(500).send(err);
     }
+
 };
 
 module.exports = api;
